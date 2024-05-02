0ee6caea1a3ff5c87c4cfc87bf6268435d28aa33
OmarMunguiaC <160805567+OmarMunguiaC@users.noreply.github.com>
2024-05-02 14:13:24 -0600

    Funcion de Utilidad

---
+++ E1_24_Funcion de Utilidad.py
@@ -0,0 +1,8 @@
+# Definimos una función de utilidad lineal simple
+def utility_function(x):
+    return 2 * x
+
+# Ejemplo de uso
+x_value = 5
+utility = utility_function(x_value)
+print(f"La utilidad de tener {x_value} es: {utility}")
